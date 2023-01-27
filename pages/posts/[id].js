198dec8cf963813a5beb99aa94e949ffa458d121
MartianLee <maritonlee@gmail.com>
2023-01-27 15:56:45 +0900

    :lipstick:Style post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,38 +1,46 @@
-import { google } from 'googleapis';
+import { google } from "googleapis";
+import styles from "../../styles/Post.module.css";
 
 export async function getServerSideProps({ query }) {
+  // Auth
+  const auth = await google.auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+  });
 
-    // Auth
-    const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'] });
+  const sheets = google.sheets({ version: "v4", auth });
 
-    const sheets = google.sheets({ version: 'v4', auth });
+  // Query
 
-    // Query
+  const { id } = query;
+  const range = `Sheet1!A${id}:C${id}`;
 
-    const { id } = query;
-    const range = `Sheet1!A${id}:C${id}`;
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range,
+  });
 
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.SHEET_ID,
-      range,
-    });
+  // Result
 
-    // Result
+  const [title, content] = response.data.values[0];
+  console.log(title, content);
 
-    const [title, content] = response.data.values[0];
-    console.log(title, content)
-
-    return { 
-        props: {
-            title,
-            content
-        } 
-    }
+  return {
+    props: {
+      title,
+      content,
+    },
+  };
 }
 
 export default function Post({ title, content }) {
-    return <article>
-        <h1>{title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: content }}></div>
-    </article>
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <article>
+          <h1>{title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: content }}></div>
+        </article>
+      </main>
+    </div>
+  );
 }
